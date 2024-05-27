df9f2bae9a43b3d3159ec4500b41fe913e89c9bb
songxwn <songxwn@foxmail.com>
2024-05-27 22:33:43 +0800

    增加超时

--- netbox_ipscanner.py
+++ netbox_ipscanner.py
@@ -11,7 +11,8 @@ class IpScan(Script):
     # 这里是用户界面中的可选变量！
     class Meta:
         name = "IP 扫描器"
-        description = "扫描可用前缀并更新 IPAM 中的 IP 地址"
+        description = "扫描可用前缀并更新 IPAM 中的 IP 地址及其状态"
+        job_timeout = 12000  # 脚本超时时间，单位为秒。防止因地址过多，导致脚本超时。
 
     def run(self, data, commit):
 
@@ -29,12 +30,12 @@ class IpScan(Script):
                 return data[0]
 
         nb = pynetbox.api(NETBOXURL, token=TOKEN)
-        nb.http_session.verify = False # 禁用Netbox的证书检查
+        nb.http_session.verify = False # 禁用Netbox的https证书检查
 
-        subnets = nb.ipam.prefixes.all()  #提取所有前缀，格式为 x.x.x.x/yyy
+        subnets = nb.ipam.prefixes.all()  #提取netbox里面的所有前缀，格式为 x.x.x.x/yyy
 
         for subnet in subnets:
-            if str(subnet.status) == 'Reserved': # 不扫描保留子网
+            if str(subnet.status) == 'Reserved': # 不扫描状态为保留的前缀
                 self.log_warning(f"Scan of {subnet.prefix} NOT done (is Reserved)")
                 continue
             IPv4network = ipaddress.IPv4Network(subnet)
